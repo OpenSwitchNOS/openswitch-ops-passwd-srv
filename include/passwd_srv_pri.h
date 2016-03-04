6a4d76c03eada6eea1846bfae82b8e66365b1caa
Mohamed Kahn <mohamed.kahn@hpe.com>
2016-03-03 16:47:50 -0800

    new: dev: password server infra setup - merged from the password server feature branch   - feature/ops-passwd-srv - removed unused module check in cmake added updated .gitreview
    Change-Id: Ia2952878084d8eece48fc47c4367537841e7f676
    Signed-off-by: Mohamed Kahn <mohamed.kahn@hpe.com>
    
    new: dev: Adding makefile and stub main()
    
    Change-Id: I3af992eb9286cbe7f0fcfd528e8dee594bfdda54
    Signed-off-by: Mohamed Kahn <mohamed.kahn@hpe.com>
    
    new: dev: Initial password server impl
    
    - change made to fix test failure
       - removed if-statement for calling unlink()
    - removed unused API
       - create socket API is not used - removed
    Change-Id: I3de67469976a83ce62e53ee277bb11c8760401bd
    Signed-off-by: Namsoo Kim <nkim@hpe.com>
    
    new: dev: added design and other misc docs for password server
     - changed based on review comments
     - more changes based on review comments
     - added how private key is stored
     - changed private key storage
    Change-Id: Ie01d16861b6e47639b6f196c8de9053b33e0831e
    Signed-off-by: Namsoo Kim <nkim@hpe.com>
    
    new: dev: passwd-srv implementation
    - added argument handle function
    - added check for sender & user
    
    Change-Id: I64af036196ed0edc14d87fe2013a06de6f94c6a2
    Signed-off-by: Namsoo Kim <nkim@hpe.com>

---
+++ include/passwd_srv_pri.h
@@ -0,0 +1,65 @@
+/*
+ * (c) Copyright 2016 Hewlett Packard Enterprise Development LP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef PASSWD_SRV_PRI_H_
+#define PASSWD_SRV_PRI_H_
+
+#include "passwd_srv_pub.h"
+
+#define PASSWD_PASSWORD_FILE "/etc/passwd"      /* file with user info */
+#define PASSWD_SHADOW_FILE   "/etc/shadow"      /* file with password info */
+#define PASSWD_GROUP_FILE    "/etc/group"       /* file with group info */
+#define PASSWD_LOGIN_FILE    "/etc/login.defs"  /* encryption method stored */
+
+#define PASSWD_SRV_PRI_KEY_LOC \
+    "/var/run/ops-passwd-srv/ops-passwd-srv-pri.pem" /*private key loc*/
+
+/*
+ * password server user-object datat structure
+ */
+typedef struct passwd_client
+{
+    int socket;               /* client socket descriptor */
+    passwd_srv_msg_t msg; 	  /* MSG from client */
+    struct spwd      *passwd; /* shadow file password structure */
+} passwd_client_t;
+
+enum PASSWD_error_code_e {
+    PASSWD_ERR_OK = 0x0,
+    PASSWD_ERR_UNKNOWN_ERROR,
+    PASSWD_ERR_INVALID_SOCKET,
+    PASSWD_ERR_MAX
+};
+
+/*
+ * password server internal APIs
+ */
+int process_client_request(passwd_client_t *client);
+
+int create_socket();
+void listen_socket();
+
+int validate_password(passwd_client_t *client);
+int validate_user(struct sockaddr_un *sockaddr, passwd_client_t *client);
+
+int create_and_store_password(passwd_client_t *client);
+struct spwd *find_password_info(const char *username);
+
+/*
+ * forward declaration
+ */
+
+#endif /* PASSWD_SRV_PRI_H_ */
